ad7edbb269bfecffba4cfd32e28c28a39ef1c190
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-05-01 13:44:59 +0800

    Updated Airflow

--- terraform-core/ec2_airflow_placeholder/outputs.tf
+++ terraform-core/ec2_airflow_placeholder/outputs.tf
@@ -1,20 +1,30 @@
+output "iam_instance_profile" {
+  description = "Name of the IAM instance-profile for EC2-Airflow"
+  value       = var.iam_instance_profile != "" ? var.iam_instance_profile : aws_iam_instance_profile.profile[0].name
+}
 
 output "iam_instance_profile_arn" {
-  value = aws_iam_instance_profile.profile[0].arn
+  description = "ARN of the IAM instance-profile for EC2-Airflow"
+  value       = var.iam_instance_profile != "" ? var.iam_instance_profile : aws_iam_instance_profile.profile[0].arn
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.this[0].id
 }
 
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance (empty if none launched)"
-  # if aws_instance.this has 1 element, return its public_ip, else empty
-  value = length(aws_instance.this) > 0 ? aws_instance.this[0].public_ip : ""
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.this[0].public_ip
 }
 
 output "instance_public_dns" {
-  description = "Public DNS of the EC2 instance (empty if none launched)"
-  value = length(aws_instance.this) > 0 ? aws_instance.this[0].public_dns : ""
+  description = "The public DNS of the EC2 instance"
+  value       = aws_instance.this[0].public_dns
 }
 
-output "iam_instance_profile" {
-  description = "Name of the IAM instance-profile for EC2-Airflow"
-  value       = aws_iam_instance_profile.profile[0].name
+output "ec2_private_key_pem" {
+  description = "The private key (in PEM format) generated for SSH access"
+  value       = tls_private_key.ec2_key[0].private_key_pem
+  sensitive   = true
 }
