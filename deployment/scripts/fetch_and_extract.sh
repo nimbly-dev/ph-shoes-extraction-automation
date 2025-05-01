ff065bbcea42ccd37be932a51cb78ef5bcfe6517
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-05-01 23:25:31 +0800

    Updated Airflow

--- deployment/scripts/fetch_and_extract.sh
+++ deployment/scripts/fetch_and_extract.sh
@@ -12,6 +12,8 @@ cd "$WORKDIR"
 aws s3 cp "s3://$BUCKET/$ZIPKEY" deployment.zip
 unzip -o deployment.zip
 
+find . -type f -name 'ph_shoes_airflow_scheduler.tar' -exec mv {} "$WORKDIR"/ \; || true
+
 # make sure host-mount dirs exist and are writable
 mkdir -p "$WORKDIR/dags" "$WORKDIR/logs"
 chmod 777 "$WORKDIR/dags" "$WORKDIR/logs"
