2e68c8c2cb237606c7ddc15a2bfca026cd826e4b
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-05-01 15:27:15 +0800

    Updated Airflow

--- terraform-core/ec2_airflow_placeholder/main.tf
+++ terraform-core/ec2_airflow_placeholder/main.tf
@@ -1,36 +1,45 @@
-# only create EC2 when an instance_profile was passed in
 locals {
   launch = var.iam_instance_profile != ""
 }
 
+# generate a keypair only if we are launching
 resource "tls_private_key" "ec2_key" {
   count     = local.launch ? 1 : 0
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+# import-safe EC2 KeyPair
 resource "aws_key_pair" "automation_key" {
   count      = local.launch ? 1 : 0
   key_name   = var.key_name
   public_key = tls_private_key.ec2_key[0].public_key_openssh
+
+  lifecycle {
+    ignore_changes = [ public_key ]
+  }
 }
 
+# import-safe Security Group
 resource "aws_security_group" "this" {
   count       = local.launch ? 1 : 0
   name        = "${var.instance_name}-sg"
   description = "SG for ${var.instance_name}"
+
   ingress {
     from_port   = var.ssh_port
     to_port     = var.ssh_port
     protocol    = "tcp"
     cidr_blocks = var.ssh_cidr_blocks
   }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   dynamic "ingress" {
     for_each = var.extra_ingress
     content {
@@ -40,15 +49,22 @@ resource "aws_security_group" "this" {
       cidr_blocks = ingress.value.cidr_blocks
     }
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = merge(var.tags, { Name = var.instance_name, Environment = var.environment })
+
+  lifecycle {
+    ignore_changes = [ description ]
+  }
 }
 
+# find latest Amazon Linux 2 AMI
 data "aws_ami" "amazon_linux2" {
   most_recent = true
   filter {
@@ -58,22 +74,26 @@ data "aws_ami" "amazon_linux2" {
   owners = ["137112412989"]
 }
 
+# actual EC2 instance
 resource "aws_instance" "this" {
   count                  = local.launch ? 1 : 0
   ami                    = data.aws_ami.amazon_linux2.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.automation_key[0].key_name
-  vpc_security_group_ids = length(var.vpc_security_group_ids) > 0 ? var.vpc_security_group_ids : [aws_security_group.this[0].id]
+  vpc_security_group_ids = (length(var.vpc_security_group_ids) > 0 ? var.vpc_security_group_ids : [aws_security_group.this[0].id])
   iam_instance_profile   = var.iam_instance_profile
   user_data              = templatefile("${path.module}/userdata.tpl", { AWS_REGION = var.aws_region })
+
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = [user_data]
+    ignore_changes        = [ user_data ]
   }
+
   root_block_device {
     volume_size           = 20
     volume_type           = "gp3"
     delete_on_termination = true
   }
+
   tags = merge(var.tags, { Name = var.instance_name, Environment = var.environment })
 }
--- terraform-core/ec2_airflow_placeholder/outputs.tf
+++ terraform-core/ec2_airflow_placeholder/outputs.tf
@@ -1,30 +1,32 @@
 output "iam_instance_profile" {
   description = "Name of the IAM instance-profile for EC2-Airflow"
-  value       = var.iam_instance_profile != "" ? var.iam_instance_profile : aws_iam_instance_profile.profile[0].name
+  value       = (var.iam_instance_profile != "" ?
+                var.iam_instance_profile :
+                try(aws_iam_instance_profile.profile[0].name, ""))
 }
 
 output "iam_instance_profile_arn" {
   description = "ARN of the IAM instance-profile for EC2-Airflow"
-  value       = var.iam_instance_profile != "" ? var.iam_instance_profile : aws_iam_instance_profile.profile[0].arn
+  value       = (var.iam_instance_profile != "" ? var.iam_instance_profile : try(aws_iam_instance_profile.profile[0].arn, ""))
 }
 
 output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.this[0].id
+  description = "The ID of the EC2 instance (if launched)"
+  value       = try(aws_instance.this[0].id, "")
 }
 
 output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.this[0].public_ip
+  description = "The public IP address of the EC2 instance (if launched)"
+  value       = try(aws_instance.this[0].public_ip, "")
 }
 
 output "instance_public_dns" {
-  description = "The public DNS of the EC2 instance"
-  value       = aws_instance.this[0].public_dns
+  description = "The public DNS of the EC2 instance (if launched)"
+  value       = try(aws_instance.this[0].public_dns, "")
 }
 
 output "ec2_private_key_pem" {
   description = "The private key (in PEM format) generated for SSH access"
-  value       = tls_private_key.ec2_key[0].private_key_pem
+  value       = try(tls_private_key.ec2_key[0].private_key_pem, "")
   sensitive   = true
 }
